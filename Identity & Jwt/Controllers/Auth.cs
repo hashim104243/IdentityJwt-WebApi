86ebf32e16a0fb2344bd04151895cf86ce1617b1
Muhammad Hashim <hashim104243@gmail.com>
2025-04-28 13:01:19 +0500

    implemented jwt

--- Identity & Jwt/Controllers/Auth.cs
+++ Identity & Jwt/Controllers/Auth.cs
@@ -1,9 +1,14 @@
-﻿using Identity___Jwt.context;
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Identity___Jwt.context;
 using Identity___Jwt.DTOS;
 using Identity___Jwt.IdentitModel;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Identity___Jwt.Controllers
 {
@@ -13,11 +18,13 @@ namespace Identity___Jwt.Controllers
     {
         private readonly ApplicationDbContext _context;
         private readonly UserManager<ApplicationUser> userManager;
+        private readonly IConfiguration configuration;
 
-        public Auth(ApplicationDbContext context,UserManager<ApplicationUser> userManager)
+        public Auth(ApplicationDbContext context,UserManager<ApplicationUser> userManager, IConfiguration configuration)
         {
             this._context = context;
             this.userManager = userManager;
+            this.configuration = configuration;
         }
 
         [HttpPost("Register")]
@@ -55,12 +62,39 @@ namespace Identity___Jwt.Controllers
                 return BadRequest("user has null");
             }
             var checkUser =await userManager.FindByEmailAsync(user.Email);
+            var token = GenerateJwtToken(checkUser);
             if (checkUser==null)
             {
                 return BadRequest("user must be register before login");
             }
-            return Ok(new { message = "successfuly login" });
+            return Ok(new { message = "successfuly login", toke=token });
             
+        }
+        public string GenerateJwtToken(ApplicationUser user)
+        {
+            var jwtSettings = configuration.GetSection("JwtSettings");
+
+            var claims = new[]
+            {
+            new Claim(JwtRegisteredClaimNames.Sub, user.UserName),
+            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+            new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+            // Add roles if needed
+            new Claim(ClaimTypes.Role, "DefalutUser"), // Assuming you want to add role as claim
+        };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings["SecretKey"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: jwtSettings["Issuer"],
+                audience: jwtSettings["Audience"],
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(Convert.ToDouble(jwtSettings["ExpiryInMinutes"])),
+                signingCredentials: creds
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
         }
     }
 }
--- Identity & Jwt/Program.cs
+++ Identity & Jwt/Program.cs
@@ -1,9 +1,12 @@
+using System.Text;
 using Identity___Jwt.context;
 using Identity___Jwt.IdentitModel;
 using Identity___Jwt.Seed;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,6 +19,23 @@ builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlSer
 builder.Services.AddIdentity<ApplicationUser,ApplicationRole>().AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
 builder.Services.AddScoped<DefaultUser>();
 builder.Services.AddScoped<DefaultRoles>();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        var jwtSettings = builder.Configuration.GetSection("JwtSettings");
+
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+
+            ValidIssuer = jwtSettings["Issuer"],
+            ValidAudience = jwtSettings["Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings["SecretKey"]))
+        };
+    });
 var app = builder.Build();
 using (var scope = app.Services.CreateScope())
 {
